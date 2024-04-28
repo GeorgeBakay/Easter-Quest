570f0a0027e4693e3c9dc73464dd374ea821c577
BakayGeorge <georgebakay125@gmail.com>
2024-04-28 22:53:19 +0300

    ad d TileMap

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
+const SPEED = 50.0
 @onready var anim = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -16,17 +16,18 @@ func _physics_process(delta):
 	if Xdirection:
 		velocity.x = Xdirection * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 5.0)
+		velocity.x = move_toward(velocity.x, 0, 100.0)
 	if Ydirection:
 		velocity.y = Ydirection * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, 5.0)
+		velocity.y = move_toward(velocity.y, 0, 100.0)
+	if Xdirection or Ydirection:
+		anim.play("Run")
+	else:
+		anim.play("Idle")
 	if Xdirection == -1:
 		anim.flip_h=true
 	elif Xdirection == 1:
 		anim.flip_h = false
-	anim.play("Idle")
+	
 	move_and_slide()
-func _on_ready():
-	anim.play("Idle")
-	pass
